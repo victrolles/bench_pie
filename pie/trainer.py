a31747d716be45cde68b9d013955e06b000b4677
Victrolles <vi.goudal@gmail.com>
2023-12-01 10:19:30 +0100

    Ajout de distributed_port pour l'initialisation de deepspeed dans modeling.py. Ajout d'un tracker du throughput dans trainer.p dans modeling.py. Ajout d'un tracker du throughput dans trainer.py

--- pie/trainer.py
+++ pie/trainer.py
@@ -1,4 +1,8 @@
 from time import time
+import idr_torch
+from statistics import mean
+import csv
+import torch.distributed as dist
 
 import mlflow
 import torch
@@ -123,6 +127,7 @@ class Trainer:
     def train_loop(self) -> torch.Tensor:
         self.model.train()
         list_loss = torch.Tensor([]).to(self.device)
+        train_list_time_iter = []
         initial_iter_loop = self.config.step % self.config.total_train_step_per_epoch
         loop: tqdm[tuple[torch.Tensor, torch.Tensor, torch.Tensor]] = tqdm(
             self.train_loader,
@@ -132,6 +137,11 @@ class Trainer:
             ascii=True,
         )
         for i, (inputs, _, target) in enumerate(loop):
+            
+            start_time_iter = time()
+            if i == 0:
+                inputs_shape = inputs.shape
+
             inputs = inputs.to(self.device)
             target = target.to(self.device)
             out = self.model(input_ids=inputs)
@@ -149,6 +159,8 @@ class Trainer:
                 self.optimizer.zero_grad()
                 if self.lr_scheduler is not None:
                     self.lr_scheduler.step()
+
+            train_list_time_iter.append(time() - start_time_iter)
 
             # To monitor training
             list_loss = torch.cat((list_loss, loss.detach().data.view(1)))
@@ -173,7 +185,7 @@ class Trainer:
                 self.prof.step()
 
             if (
-                (i == 20 and self.config.debug)
+                (i == 75 and self.config.debug)
                 or (i == self.config.total_train_step_per_epoch + 1 - initial_iter_loop)
             ):
                 loop.close()
@@ -185,17 +197,29 @@ class Trainer:
 
             self.config.step += 1
 
+        # tokens/second/gpu = (seq/iter/gpu) * (tokens/seq) / (iter/second)
+        train_tokens_s = inputs_shape[0] * inputs_shape[1] / mean(train_list_time_iter[:25])
+        print(f"Train token per second of GPU n°{idr_torch.rank}: {train_tokens_s:.1f}")
+        tensor_train_tokens_s = torch.tensor(train_tokens_s, device=self.device)
+        # Sum the throughput of all GPUs
+        dist.all_reduce(tensor_train_tokens_s, op=dist.ReduceOp.SUM)
+        self.train_tokens_s = tensor_train_tokens_s.item()
+
         return list_loss
 
     def eval_loop(self) -> float:
         self.metric.reset()
         self.model.eval()
+        list_tokens_s_per_gpu = []
         loop: tqdm[tuple[torch.Tensor, torch.Tensor, torch.Tensor]] = tqdm(
             self.valid_loader, disable=(self.rank != 0), ascii=True
         )
 
         with torch.no_grad():
             for i, (inp_ids, mask, targets) in enumerate(loop):
+
+                start_time_iter = time()
+
                 inp_ids = inp_ids.to(self.device)
                 mask = mask.to(self.device)
                 targets = targets.to(self.device)
@@ -208,16 +232,25 @@ class Trainer:
                 )
                 avg_perplexiy: torch.Tensor = self.metric.compute()
 
+                list_tokens_s_per_gpu.append(inp_ids.shape[0] * inp_ids.shape[1] / (time() - start_time_iter))
+
                 loop.set_postfix(
                     average_perplexity=avg_perplexiy.item(),
                     perplexity=score.item(),
                 )
 
-                if i == 20 and self.config.debug:
+                if i == 75 and self.config.debug:
                     loop.close()
                     break
 
             perplexity = self.metric.compute().item()
+
+            val_tokens_s = mean(list_tokens_s_per_gpu[:25])
+            print(f"Inference token per second of GPU n°{idr_torch.rank}: {val_tokens_s:.1f}")
+            tensor_val_tokens_s = torch.tensor(val_tokens_s, device=self.device)
+            dist.all_reduce(tensor_val_tokens_s, op=dist.ReduceOp.SUM)
+            self.val_tokens_s = tensor_val_tokens_s.item()
+
             if self.config.track:
                 mlflow.log_metric("perplexity", perplexity, step=self.config.step)
 
@@ -261,6 +294,48 @@ class Trainer:
                 f"perplexity: {perplexity:.3f}\n"
             )
             print_rank_0("#" * 60, "\n")
+
+        if idr_torch.rank==0:
+
+            print(
+                f"\n------------------- results benckmarks -------------------",
+                f"\nGPU type : {torch.cuda.get_device_name(self.device)}",
+                f"\nGPU number : {idr_torch.size}",
+                f"\nModel name : {self.config.model_name}",
+                f"\nOptimized DDP : {self.config.training_dist}",
+                f"\nDeepspeed ZeRO stage: {self.config.stage}",
+                f"\nTraining batch size : {self.config.batch_size}",
+                f"\nInference batch size : {self.config.valid_batch_size}",
+                f"\nSequence length: {self.config.seq_length}",
+                f"\nNumber of layer freezed : {self.config.nb_layer_freezed}",
+                f"\nEpoch duration : {(time() - start_epoch):.2f}s",
+                f"\nGlobal Training Token/s : {self.train_tokens_s}",
+                f"\nGlobal Inference Token/s : {self.val_tokens_s}",
+                f"\nGlobal Max Memory Allocated : {round(torch.cuda.max_memory_allocated(self.device)/2**30, 2):.2f}",
+                f"\nGlobal Max Memory Reserved : {round(torch.cuda.max_memory_reserved(self.device)/2**30, 2):.2f}",
+                f"\nAverage loss : {list_loss.mean().item():.3f}",
+                f"\nPerplexity : {perplexity}",
+                f"\n----------------------------------------------------------\n")
+
+            with open("../../bench_pie.csv",'a') as file:
+                writer=csv.writer(file)
+                # typeGPU,nbrGPU,model_name,training_dist,stage,batch_size,valid_batch_size,seq_length,nb_layer_freezed,epoch_duration,train_tokens_s,val_tokens_s,maxMemAlloc,maxMemRes,mean_loss,perplexity
+                writer.writerow([torch.cuda.get_device_name(self.device),
+                                idr_torch.size,
+                                self.config.model_name,
+                                self.config.training_dist,
+                                self.config.stage,
+                                self.config.batch_size,
+                                self.config.valid_batch_size,
+                                self.config.seq_length,
+                                self.config.nb_layer_freezed,
+                                (time() - start_epoch),
+                                self.train_tokens_s,
+                                self.val_tokens_s,
+                                round(torch.cuda.max_memory_allocated(self.device)/2**30, 2),
+                                round(torch.cuda.max_memory_reserved(self.device)/2**30, 2),
+                                list_loss.mean().item(),
+                                perplexity])   
 
         if self.config.checkpoint:
             save_checkpoints(self.config, self.model, self.optimizer, self.lr_scheduler)
